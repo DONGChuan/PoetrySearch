dc3fa2abf333b6c74e36def221dd20f81de4dd26
dchuan <chuan.dong@amadeus.com>
2016-03-14 21:05:57 +0100

    Add one to many relation

--- src/main/java/com/dong/web/model/Poet.java
+++ src/main/java/com/dong/web/model/Poet.java
@@ -1,6 +1,7 @@
 package com.dong.web.model;
 
 import javax.persistence.*;
+import java.util.Set;
 
 /**
  * Created by dchuan on 2016/3/14.
@@ -11,12 +12,20 @@ public class Poet {
 
     // Fields
 
-    @Id
+    @Id @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
     @Column(name = "name", nullable = false)
     private String name;
+
+    @OneToMany(targetEntity = Poetry.class)
+    @JoinColumn(name = "poet_id", referencedColumnName = "poet_id")
+    private Set<Poetry> poetries;
+
+    // Constructor
+
+    public Poet() {}
 
     // Setters and Getters
 
@@ -34,5 +43,13 @@ public class Poet {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Poetry> getPoetries() {
+        return poetries;
+    }
+
+    public void setPoetries(Set<Poetry> poetries) {
+        this.poetries = poetries;
     }
 }
